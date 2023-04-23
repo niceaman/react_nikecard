8043ee84049bf1e365db679d03c9a2982d1f6f82
jarkrapob <jarkrapob.s@gmail.com>
2023-04-23 20:18:56 +0700

    up card

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ function App() {
   return (
     <>
       <div className="container">
+        <div className="hiBox">
+
+          <div className="cardContainer">
+            <h2>NIKE</h2>
+          </div>
+
+
+
+
+          <div>
+            <h2>NIKE AIR MAX 270</h2>
+          </div>
+          <div>
+            <h2>NIKE AIR MAX 270</h2>
+
+          </div>
+
+        </div>
+        <div className="loBox">
+
+        </div>
 
       </div>
     </>
