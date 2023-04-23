1399903f6726fb8264a92e96a8fd341789a44e90
jarkrapob <jarkrapob.s@gmail.com>
2023-04-23 18:56:43 +0700

    up : 'init'

---
+++ main.js
@@ -0,0 +1,15 @@
+// Import components
+function App() {
+  return (
+    <>
+      <div className="container">
+
+      </div>
+    </>
+  );
+}
+
+// Render
+const domRoot = document.getElementById("root");
+const root = ReactDOM.createRoot(domRoot);
+root.render(<App />);
